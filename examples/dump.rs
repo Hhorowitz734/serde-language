6ba10ed012c90b8b242fb577f888ec40a61f0231
Benjamin_Horowitz <hhorowitz734@gmail.com>
2024-08-21 11:43:03 -0500

    created new test file, added tree pretty print implementation for our tree case

--- examples/dump.rs
+++ examples/dump.rs
@@ -8,7 +8,7 @@ use std::io::Read;
 fn main() {
 
     // Open the JSON file
-    let mut file = File::open("/Users/bhorowitz/Documents/serde-language/examples/sample-files/input-schemas/csv.json").expect("Unable to open file");
+    let mut file = File::open("/Users/bhorowitz/Documents/serde-language/examples/sample-files/input-schemas/rowlf.json").expect("Unable to open file");
 
     // Read the file contents into a string
     let mut contents = String::new();
@@ -18,8 +18,8 @@ fn main() {
     let field: Field = serde_json::from_str(&contents).expect("Unable to parse JSON");
 
     // Print the deserialized Field instance
-    println!("Deserialized (Debug format): {:#?}", field);
-
+    //println!("Deserialized (Debug format): {:#?}", field);
+    field.pretty_print(0);
 
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,45 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Field {
+
     #[serde(rename = "Name")]
     pub name: String,
+
+    #[serde(rename = "Identifier")]
+    pub identifier: String,
+
     #[serde(rename = "Delimiter")]
     pub delim: String,
+
     #[serde(rename = "Subfields")]
     pub subfields: Option<Vec<Field>>, // Optional subfields
 }
+
+
+impl Field {
+
+    pub fn pretty_print(&self, indent: usize) {
+        /*
+        * Pretty print fields as a tree. Maybe we want a package to do this in the future.
+        * This is just a testing function, as it is performance intensive
+        */
+
+        let indentation = " ".repeat(indent);
+        println!("{}- {} (Delimiter: \"{}\" | Identifier: \"{}\")", indentation, self.name, self.delim, self.identifier);
+
+        // Recursive case
+        if let Some(ref subfields) = self.subfields {
+            for sf in subfields {
+                sf.pretty_print(indent + 2); // Recursive call
+            }
+        }
+
+        // The base case is just that it falls through here.
+
+    }
+
+}
+
 
 
 #[derive(Debug)]
